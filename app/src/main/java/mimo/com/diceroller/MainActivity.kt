15659da83a8fd0565efc794894ff4973b286739c
Ajay <ajaylottey@gmail.com>
2020-11-28 20:14:08 +0100

    :sparkles: change image resource with random integer

--- app/src/main/java/mimo/com/diceroller/MainActivity.kt
+++ app/src/main/java/mimo/com/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package mimo.com.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mimo.com.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +15,23 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    binding.rollButton.setOnClickListener {
+        diceRoller()
+    }
 
+    }
 
+    private fun diceRoller() {
+
+        val imageResource = when (Random.nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        binding.diceImage.setImageResource(imageResource)
     }
 }
